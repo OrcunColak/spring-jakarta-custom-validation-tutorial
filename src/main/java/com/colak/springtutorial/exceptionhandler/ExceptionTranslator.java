7fecb3813204428601f87f70024238495a7c84c5
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-18 14:41:04 +0300

    add example ProblemDetails

--- src/main/java/com/colak/springtutorial/exceptionhandler/ExceptionTranslator.java
+++ src/main/java/com/colak/springtutorial/exceptionhandler/ExceptionTranslator.java
@@ -16,8 +16,23 @@ import java.util.Set;
 public class ExceptionTranslator extends ResponseEntityExceptionHandler {
 
     @Builder
-    private record InvalidatedParams (String cause, String attribute) {}
+    private record InvalidatedParams(String cause, String attribute) {
+    }
 
+    // HTTP/1.1 403 Forbidden
+    // Content-Type: application/problem+json
+    // Content-Language: en
+    //
+    // {
+    //   "status": 403,
+    //   "detail": "Your current balance is 30, but that costs 50.",
+    //   "type": "https://example.com/errors/out-of-credit",
+    //   "title": "You do not have enough credit.",
+    //   "instance": "/account/12345",
+    //   "traceId": "acbd0001-7b8e-4e81-9d6b-3e34ce7c9a9e", // optional field
+    //   "balance": 30, // additional information
+    //   "cost": 50 // additional information
+    // }
     @ExceptionHandler(ConstraintViolationException.class)
     ProblemDetail handleConstraintViolationException(ConstraintViolationException constraintViolationException) {
         Set<ConstraintViolation<?>> errors = constraintViolationException.getConstraintViolations();
@@ -29,6 +44,9 @@ public class ExceptionTranslator extends ResponseEntityExceptionHandler {
                 ).toList();
 
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Request validation failed");
+        // problemDetail.setType();
+        // problemDetail.setInstance();
+
         problemDetail.setTitle("Validation Failed");
         problemDetail.setProperty("invalidParams", validationResponse);
         return problemDetail;
